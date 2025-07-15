77e778bda37a3e52818a5df005e7f4b8dab5e828
truple98 <ljw122321@gmail.com>
2025-07-15 10:57:35 +0900

    remove dist

--- backend/dist/controllers/analyticsController.d.ts
+++
@@ -1,3 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare const getAnalyticsController: (req: Request, res: Response, next: NextFunction) => Promise<void>;
-//# sourceMappingURL=analyticsController.d.ts.map
